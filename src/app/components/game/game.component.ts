99ec306e45e41ae6656549f452309a3c0a7afe19
Matthew Green <greeny88@gmail.com>
2021-06-17 15:25:20 -0500

    Cleaned up comments and left some more todos for later.

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,16 +7,15 @@ import template from './game.html';
     template
 })
 export class GameComponent {
-    // TODO: keep track of different car colors
-    // TODO: calculate score
     // TODO: allow removal of car from color
-    // TODO: reset color counts
+    // TODO: reset all color counts
     colors: string[];
     colorValues: Object;
     count: Object;
     score: number;
 
     constructor() {
+        // TODO: Make this configurable (because kids like teal and pink for some reason)
         this.colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
         this.count = {};
         // TODO: Make this configurable
@@ -39,17 +38,15 @@ export class GameComponent {
 
     addColor(color: string) {
         this.count[color] += 1;
+        // TODO: save counts in indexeddb for safe keeping
         this.calculateScore();
     }
 
     private calculateScore() {
-        // let rainbows = 99;
         this.score = 0;
         for (let color in this.count) {
             this.score += (color in this.colorValues) ? this.colorValues[color] * this.count[color] : 1 * this.count[color];
-            // rainbows = (this.count[color] < rainbows) ? this.count[color] : rainbows;
         }
-        // Math.min(...Object.values(this.count));
         this.score += Math.min(...Object.values(this.count)) * 10; // value of each rainbow is 10
     }
 }
