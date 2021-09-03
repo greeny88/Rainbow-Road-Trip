23d684f6cbe15f6cdc9309fe8c86a0135fe09a1a
Matthew Green <greeny88@gmail.com>
2021-09-02 20:59:21 -0500

    Started on config settings.

--- src/app/components/config.service.ts
+++ src/app/components/config.service.ts
@@ -4,33 +4,34 @@ import { IDBService } from './idb.service';
 
 @Injectable()
 export class ConfigService {
-    private colorConfig: Object;
+    private config: Object;
 
     constructor(private idb: IDBService) {}
 
     ngOnInit() {
         // TODO: wait for return data?
-        this.colorConfig = this.idb.getConfigData();
+        this.config = this.idb.getConfigData();
 
-        if (this.colorConfig === undefined) {
+        if (this.config === undefined) {
             // default config
-            this.colorConfig = {
+            this.config = {
                 'red': 1,
                 'orange': 3,
                 'yellow': 2,
                 'green': 2,
                 'blue': 1,
-                'purple': 3
+                'purple': 3,
+                'rainbowValue': 10
             }
         }
     }
 
-    getColorSettings() {
-        return this.colorConfig;
+    getColorSettings(): Object {
+        return this.config;
     }
 
-    setColorSettings(config) {
-        this.colorConfig = config;
+    setColorSettings(config: Object) {
+        this.config = config;
     }
 
 }
