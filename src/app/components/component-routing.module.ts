e469d528a2324f4b1acaedaa077a20a30865139a
Matthew Green <greeny88@gmail.com>
2023-08-26 19:47:13 -0500

    Trying to fix deployment.

--- src/app/components/component-routing.module.ts
+++ src/app/components/component-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PageNotFoundComponent } from './page-not-found.component';
-
 const routes: Routes = [
     {
         path: 'game',
@@ -29,9 +27,6 @@ const routes: Routes = [
     ],
     exports: [
         RouterModule
-    ],
-    declarations: [
-        PageNotFoundComponent
     ]
 })
 export class ComponentRoutingModule {}
